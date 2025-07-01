6bfae50e6627ea24c0f20a54fea6fbf4f8ac0b8c
Patrick Lehmann <Paebbels@gmail.com>
2025-07-02 01:22:05 +0200

    Fixed embedded '{' and '}' characters in log lines.

--- pyEDAA/OutputFilter/CLI/Vivado.py
+++ pyEDAA/OutputFilter/CLI/Vivado.py
@@ -160,39 +160,40 @@ class VivadoHandlers(metaclass=ExtendedType, mixin=True):
 
 	def ColoredOutput(self, lines: Iterable[Line]) -> None:
 		for i, line in enumerate(lines, start=1):
+			message = str(line).replace("{", "{{").replace("}", "}}")
 			if isinstance(line, ProcessorException):
-				print(f"{i:4}: {{RED}}EXCEPTION:{{NOCOLOR}} {line}".format(**self.Foreground))
+				print(f"{i:4}: {{RED}}EXCEPTION:{{NOCOLOR}} {message}".format(**self.Foreground))
 			elif line.Kind is LineKind.Normal:
 				print(f"{i:4}: {line.Message}")
 			elif LineKind.Message in line.Kind:
 				if line.Kind is LineKind.InfoMessage:
-					print(f"{i:4}: {{BLUE}}{line}{{NOCOLOR}}".format(**self.Foreground))
+					print(f"{i:4}: {{BLUE}}{message}{{NOCOLOR}}".format(**self.Foreground))
 				elif line.Kind is LineKind.WarningMessage:
-					print(f"{i:4}: {{YELLOW}}{line}{{NOCOLOR}}".format(**self.Foreground))
+					print(f"{i:4}: {{YELLOW}}{message}{{NOCOLOR}}".format(**self.Foreground))
 				elif line.Kind is LineKind.CriticalWarningMessage:
-					print(f"{i:4}: {{MAGENTA}}{line}{{NOCOLOR}}".format(**self.Foreground))
+					print(f"{i:4}: {{MAGENTA}}{message}{{NOCOLOR}}".format(**self.Foreground))
 				elif line.Kind is LineKind.ErrorMessage:
-					print(f"{i:4}: {{RED}}{line}{{NOCOLOR}}".format(**self.Foreground))
+					print(f"{i:4}: {{RED}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif LineKind.TclCommand in line.Kind:
-				print(f"{i:4}: {{CYAN}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{CYAN}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif (LineKind.Start in line.Kind) or (LineKind.End in line.Kind):
-				print(f"{i:4}: {{DARK_CYAN}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{DARK_CYAN}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif line.Kind is LineKind.ParagraphHeadline:
-				print(f"{i:4}: {{DARK_YELLOW}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{DARK_YELLOW}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif LineKind.Table in line.Kind:
-				print(f"{i:4}: {{WHITE}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{WHITE}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif LineKind.Delimiter in line.Kind:
-				print(f"{i:4}: {{GRAY}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{GRAY}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif LineKind.Verbose in line.Kind:
-				print(f"{i:4}: {{DARK_GRAY}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{DARK_GRAY}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif LineKind.Success in line.Kind:
-				print(f"{i:4}: {{GREEN}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{GREEN}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif line.Kind is LineKind.Empty:
 				print(f"{i:4}:")
 			elif line.Kind is LineKind.ProcessorError:
-				print(f"{i:4}: {{RED}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{RED}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			elif line.Kind is LineKind.Unprocessed:
-				print(f"{i:4}: {{DARK_RED}}{line}{{NOCOLOR}}".format(**self.Foreground))
+				print(f"{i:4}: {{DARK_RED}}{message}{{NOCOLOR}}".format(**self.Foreground))
 			else:
 				print(f"{i:4}: Unknown LineKind '{line._kind}' for line {line._lineNumber}.")
 				print(line)
