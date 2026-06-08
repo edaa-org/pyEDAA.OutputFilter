337545d88861bccea56b48411b6bd82d41ee0242
Patrick Lehmann <Paebbels@gmail.com>
2026-06-08 08:44:29 +0200

    Bumped version to v0.9.0.

--- run.ps1
+++ run.ps1
@@ -33,7 +33,7 @@ Param(
 )
 
 $PackageName = "pyEDAA.OutputFilter"
-$PackageVersion = "0.8.0"
+$PackageVersion = "0.9.0"
 
 # set default values
 $EnableDebug =        [bool]$PSCmdlet.MyInvocation.BoundParameters["Debug"]
